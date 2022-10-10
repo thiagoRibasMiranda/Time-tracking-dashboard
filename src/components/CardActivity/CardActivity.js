e4b58f7ee8d2816cf9a843c13e5f8229a3bd88f8
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2022-10-10 12:24:59 -0300

    test: created test renders the component CardActivity

---
+++ src/components/CardActivity/CardActivity.js
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react';
+import CardActivity from './CardActivity';
+
+test('renders the component CardActivity', async () => {
+  const activityData = {
+    title: "Social",
+    current: 5,
+    previous: 10
+  };
+
+  render(<CardActivity data={ activityData }/>);
+
+  const titleActivity = screen.getByText(/Social/);
+  expect(titleActivity).toBeInTheDocument();
+
+  const currentActivityTime = screen.getByText(/5hrs/);
+  expect(currentActivityTime).toBeInTheDocument();
+
+  const previousActivityTime = screen.getByText(/Last Week - 10hrs/);
+  expect(previousActivityTime).toBeInTheDocument();
+
+});
