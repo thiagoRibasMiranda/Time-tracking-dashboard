28831d60e5222eac4b702627e4367820b2bdb9af
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2022-10-11 10:37:02 -0300

    feat: implemented the component CardActivity

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import CardUser from './components/CardUser/CardUser';
+import CardActivity from './components/CardActivity/CardActivity';
 
 function App() {
+
   const userName = "Thiago Ribas"
+  const activityData = {
+    title: "Social",
+    current: 5,
+    previous: 10,
+    period: "Last Week"
+  };
+
   return (
     <div>
       <CardUser name={userName} data-testid="card-user" />
+      <CardActivity activityData={activityData} data-testid="card-activities" />
     </div>
   );
 }
