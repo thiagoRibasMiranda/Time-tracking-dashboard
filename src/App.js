e2ea7082216137708c38cdaf0c0a1378801d863c
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2022-10-13 20:37:32 -0300

    feat:  added getData function

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 import CardUser from './components/CardUser/CardUser';
 import CardActivity from './components/CardActivity/CardActivity';
 
 function App() {
-
-  const userName = "Thiago Ribas"
-  const activityData = {
-    title: "Social",
-    current: 5,
-    previous: 10,
-    period: "Last Week"
-  };
-
+  const [userName] = useState("Thiago Ribas")
+  const [data, setData] = useState([]);
+  const [time, setTime] = useState("");
+  const [period, setPeriod] = useState("");
+  const getData = () => {
+    fetch('./data.json')
+      .then((response) => response.json())
+      .then((json) => {
+        setData(json)
+      });
+  }
+  useEffect(() => {
+    getData()
+    setTime("weekly")
+    setPeriod("Last Week")
+  }, [])
+  
   return (
     <div>
       <CardUser name={userName} data-testid="card-user" />
-      <CardActivity activityData={activityData} data-testid="card-activities" />
+      {
+        data.map((d, index) => 
+          <CardActivity
+            title={d.title}
+            time={d.timeframes[time]}
+            period = {period}
+            data-testid="card-activities"
+            key={index}
+          />
+        )
+      }
     </div>
   );
 }
