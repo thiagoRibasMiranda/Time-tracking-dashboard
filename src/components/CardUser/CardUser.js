313399401b3714eef9fec99e82305cc9c4107171
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2022-10-17 13:33:44 -0300

    style:  added some style to CardUser

--- src/components/CardUser/CardUser.js
+++ src/components/CardUser/CardUser.js
@@ -1,13 +1,17 @@
-// import './CardUser.css';
+import './CardUser.css';
+import userImg from './images/image-jeremy.png'
 
 function CardUser({ name, onPress }) {
   return (
-    <div>
-      <div>
-        <p>Report for</p>
-        <p>{name}</p>
+    <div className='CardUser'>
+      <div className='UserData'>
+        <img src={userImg} alt="UserImg"/>
+        <div>
+          <p>Report for</p>
+          <h1>{name}</h1>
+        </div>
       </div>
-      <div>
+      <div className='MenuButtons'>
         <button type="button" name="daily" onClick={onPress}>Daily</button>
         <button type="button" name="weekly" onClick={onPress}>Weekly</button>
         <button type="button" name="monthly" onClick={onPress}>Monthly</button>
